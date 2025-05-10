0f5d3a2b20a8fdd35d2bf1aaf477b8cfa573ab73
Dzmitry Talkach <chakmidlot@gmail.com>
2025-05-11 00:05:30 +0200

    Increase coverage

--- tests/vabaclient/test_client.py
+++ tests/vabaclient/test_client.py
@@ -8,8 +8,30 @@ from vabaclient.client import VabaClient
 from pytest_httpx import HTTPXMock
 
 
+@pytest.mark.parametrize(
+    ("available_times", "expected"),
+    [
+        (
+            [], []
+        ),
+        (
+                {
+                    '08:00': 0,
+                    '09:00': 1,
+                    '17:00': 10,
+                    '17:20': 0,
+                    '23:20': 5
+                },
+                [
+                    {'timestamp': datetime(2025, 1, 2, 9, 0), 'count': 1},
+                    {'timestamp': datetime(2025, 1, 2, 17, 0), 'count': 10},
+                    {'timestamp': datetime(2025, 1, 2, 23, 20), 'count': 5},
+                ]
+        ),
+    ]
+)
 @pytest.mark.asyncio
-async def test_get_available_times(httpx_mock: HTTPXMock):
+async def test_get_available_times(available_times, expected, httpx_mock: HTTPXMock):
     httpx_mock.add_response(
         url="https://wellness.vs.sparkleapp.sparkle.plus/proxy.php"
             "?language=en"
@@ -35,13 +57,7 @@ async def test_get_available_times(httpx_mock: HTTPXMock):
                     '17:20': 27,
                     '23:20': 0,
                 },
-                'uhrzeiten': {
-                    '08:00': 0,
-                    '09:00': 1,
-                    '17:00': 10,
-                    '17:20': 0,
-                    '23:20': 5,
-                }
+                'uhrzeiten': available_times
             },
         }
     )
@@ -52,15 +68,58 @@ async def test_get_available_times(httpx_mock: HTTPXMock):
 
     available_times = await client.get_available_times(d)
 
-    assert available_times == [
-        {'timestamp': datetime(2025, 1, 2, 9, 0), 'count': 1},
-        {'timestamp': datetime(2025, 1, 2, 17, 0), 'count': 10},
-        {'timestamp': datetime(2025, 1, 2, 23, 20), 'count': 5},
+    assert available_times == expected
+
+
+@pytest.mark.parametrize(
+    ("reservations", "expected"),
+    [
+        (
+            '''
+                <p>There are currently no treatments booked for you.</p>
+                <div style="clear:both"></div>
+            ''',
+            []
+        ),
+        (
+            """
+                <hr>
+                <h3>Juni 2025</h3>
+                <li class="anwendungswrap" id="TicketingTermine_ID_100500">
+                    <span aria-hidden="true" class="spkl-datum kalender-style-datum">
+                        <span class="dayname">Sa</span>
+                        <span class="day">21</span>
+                        <span class="month">Juni</span>
+                        <span class="year">2025</span>
+                    </span>
+                    <div class="anwendungscontent">
+                        <div class="terminHeading">
+                            <span class="artikel">reservation   03.02.2025 from 09:00    to 09:20   for Sobaka Ulybaka</span><br>
+                            <span class="uhrzeit">Montag, 03.02.2025, <span class="spkl-secondaryTextColor">
+                                <span class="Uhrzeit">09:00</span>
+                            </span>
+                        </div>
+                        <div></div>
+                    </div>
+                    <div class="anwendungszusatzinfo">
+                        <div class="buttons" style="flex-grow: 1;">
+                            <button type="button" data-spkl-click="userTicketingTermine.showMoveTicketingTerminDialog(2316842)">Re-schedule</button>
+                            <button type="button" data-spkl-click="userTicketingTermine.showVoucher(2316842)">Show ticket</button>
+                            <button type="button" data-spkl-click="userTicketingTermine.showCode(2316842)">Show code</button>
+                        </div>
+                        <div class="anmerkungen" style="align-self: flex-end;"></div>
+                    </div>
+                </li>
+                <div style="clear:both"></div>
+            """,
+            [
+                {'id': 100500, 'timestamp': datetime(2025, 2, 3, 9, 0)}
+            ]
+        )
     ]
-
-
+)
 @pytest.mark.asyncio
-async def test_get_active_appointments(httpx_mock: HTTPXMock):
+async def test_get_active_appointments(reservations, expected, httpx_mock: HTTPXMock):
     httpx_mock.add_response(
         url='https://wellness.vs.sparkleapp.sparkle.plus/proxy.php'
             '?key=ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -68,36 +127,7 @@ async def test_get_active_appointments(httpx_mock: HTTPXMock):
             '&apikey=43816A1657EC4FCB6E953B5BA3EEEen'
             '&modul=sparkleTicketingOnline'
             '&file=userTermine.php',
-        text="""
-<hr>
-<h3>Juni 2025</h3>
-<li class="anwendungswrap" id="TicketingTermine_ID_100500">
-    <span aria-hidden="true" class="spkl-datum kalender-style-datum">
-        <span class="dayname">Sa</span>
-        <span class="day">21</span>
-        <span class="month">Juni</span>
-        <span class="year">2025</span>
-    </span>
-    <div class="anwendungscontent">
-        <div class="terminHeading">
-            <span class="artikel">reservation   03.02.2025 from 09:00    to 09:20   for Sobaka Ulybaka</span><br>
-            <span class="uhrzeit">Montag, 03.02.2025, <span class="spkl-secondaryTextColor">
-                <span class="Uhrzeit">09:00</span>
-            </span>
-        </div>
-        <div></div>
-    </div>
-    <div class="anwendungszusatzinfo">
-        <div class="buttons" style="flex-grow: 1;">
-            <button type="button" data-spkl-click="userTicketingTermine.showMoveTicketingTerminDialog(2316842)">Re-schedule</button>
-            <button type="button" data-spkl-click="userTicketingTermine.showVoucher(2316842)">Show ticket</button>
-            <button type="button" data-spkl-click="userTicketingTermine.showCode(2316842)">Show code</button>
-        </div>
-        <div class="anmerkungen" style="align-self: flex-end;"></div>
-    </div>
-</li>
-<div style="clear:both"></div>
-    """)
+        text=reservations)
 
     client = VabaClient("test_user", "test_password")
 
@@ -108,9 +138,7 @@ async def test_get_active_appointments(httpx_mock: HTTPXMock):
 
     login_mock.assert_called_once()
 
-    assert appointments == [
-        {'id': 100500, 'timestamp': datetime(2025, 2, 3, 9, 0)}
-    ]
+    assert appointments == expected
 
 
 @pytest.mark.asyncio
@@ -140,6 +168,22 @@ async def test_update_appointment_time(httpx_mock: HTTPXMock):
 
 
 @pytest.mark.asyncio
+async def test_update_appointment_time_fails(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(
+        method="POST",
+        json={'success': False}
+    )
+
+    client = VabaClient("test_user", "test_password")
+
+    login_mock = AsyncMock(return_value="ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    client._get_login_token = login_mock
+
+    with pytest.raises(Exception, match="Can't update appointment"):
+        await client.update_appointment_time(100500, datetime(2025, 3, 4, 12, 40))
+
+
+@pytest.mark.asyncio
 async def test_login(httpx_mock: HTTPXMock):
     httpx_mock.add_response(
         url='https://wellness.vs.sparkleapp.sparkle.plus/proxy.php'
@@ -158,4 +202,16 @@ async def test_login(httpx_mock: HTTPXMock):
         return population[:k]
 
     with mock.patch('random.choices', choice_firsts):
+        await client._get_login_token()
+
+
+@pytest.mark.asyncio
+async def test_login_failed(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(
+        json={'success': False}
+    )
+
+    client = VabaClient("test_user", "test_password")
+
+    with pytest.raises(Exception, match="Can't login"):
         await client._get_login_token()
